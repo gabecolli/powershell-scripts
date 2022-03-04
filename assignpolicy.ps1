822081607e655bd6897eda83c727c782de44de4a
gcolli <80380712+gcolli@users.noreply.github.com>
2022-03-04 09:19:27 -0800

    Delete assignpolicy.ps1

--- assignpolicy.ps1
+++
@@ -1,2 +0,0 @@
-$policy = Get-AzPolicyDefinition | where-object {$_.properties.displayname -eq 'Allowed resource types'}
-New-AzPolicyAssignment -Name "test-policy-assignment" -PolicyDefinition $policy -Scope "/subscriptions/your sub id/resourceGroups/demo" -listOfResourceTypesAllowed 'filepath.json'
