fad2f24e93f559d53b7883f666200f520e71ba47
gcolli <80380712+gcolli@users.noreply.github.com>
2022-09-29 16:18:20 -0500

    Update send email using PScred object.ps1
    removed emails

--- send email using PScred object.ps1
+++ send email using PScred object.ps1
@@ -1,2 +1,2 @@
 $mycred = Get-AutomationPSCredential -Name 'noreply'
-Send-MailMessage -From 'User01 colliga.test@outlook.com' -To 'ITGroup gcolli795@gmail.com' -Subject "Don't forget today's meeting!" -Credential $mycred -SmtpServer 'smtp-mail.outlook.com' -UseSsl
+Send-MailMessage -From 'User01 email@outlook.com' -To 'ITGroup email@gmail.com' -Subject "Don't forget today's meeting!" -Credential $mycred -SmtpServer 'smtp-mail.outlook.com' -UseSsl
