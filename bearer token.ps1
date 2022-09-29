5aaff1d9744162611de3c8a5a66c602bea593ad9
gcolli <80380712+gcolli@users.noreply.github.com>
2022-09-29 16:13:28 -0500

    Update bearer token.ps1
    removed secrets

--- bearer token.ps1
+++ bearer token.ps1
@@ -1,6 +1,6 @@
-﻿$AppID    = '3513f4b6-af10-46e1-a179-6b3e897c24b8'
-$Secret   = 'Mmi8Q~4nyH1yH66wSHCSQe2-0kWxvFTnuIU1AbrY'
-$TenantID = '8582505d-c91e-40e2-823f-0761a18048a5'
+﻿$AppID    = '****'
+$Secret   = '****'
+$TenantID = '****'
 $Resource = "https://management.azure.com/"
 $TokenUri = "https://login.microsoftonline.com/$TenantID/oauth2/token/"
 $Body     = "client_id=$AppId&client_secret=$Secret&resource=$Resource&grant_type=client_credentials"
